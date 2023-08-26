c3dd5301d4aa3d04f22a8f760db0caa210987b0e
Karen Grigoryan <karen@karenjs.com>
2023-08-26 17:15:11 +0200

    feat: add wrapping

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,5 @@ vim.opt.winbar = "%=%m %f"
 vim.cmd("autocmd User TelescopePreviewerLoaded setlocal number")
 vim.opt.conceallevel = 0 -- Do not hide * markup for bold and italic
 vim.opt.relativenumber = false -- Show relative line numbers
+vim.opt.wrap = true -- Enable line wrap
+vim.opt.breakindent = true -- Keep indentation on wrapped lines
