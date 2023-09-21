fe212203c844370d068cdd6302f2e09507fd4828
Karen Grigoryan <karen@karenjs.com>
2023-09-21 21:34:45 +0200

    feat: add jinja and test/src toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -67,3 +67,41 @@ vim.keymap.del({ "n", "s", "x" }, "<C-s>")
 -- move faster with shift
 vim.keymap.set({ "n", "v" }, "<C-j>", "5j", { noremap = true, silent = true, desc = "Jump 5 lines down" })
 vim.keymap.set({ "n", "v" }, "<C-k>", "5k", { noremap = true, silent = true, desc = "Jump 5 lines up" })
+
+-- Remap Ctrl-^ to switch between alternate test and source files
+vim.keymap.set(
+  "n",
+  "<C-^>",
+  ":call SwitchSrcTestFile()<CR>",
+  { noremap = true, silent = true, desc = "Switch Src/Test File" }
+)
+
+vim.api.nvim_exec(
+  [[
+  function! SwitchSrcTestFile()
+    let current_file = expand("%")
+    let alternate_file = substitute(
+      \ current_file,
+      \ '\v(\w+)(\.spec|\.test)?(\.\w+)$',
+      \ '\=submatch(1) . (empty(submatch(2)) ? GetFileExtension() : "") . submatch(3)',
+      \ ""
+    \)
+    if alternate_file != "" 
+      execute "e " . alternate_file
+    else
+      normal! <C-^>
+    endif
+  endfunction
+
+  function! GetFileExtension()
+    if filereadable(resolve(expand("%:.:h") . "/" . submatch(1) . ".spec" . submatch(3)))
+      return ".spec"
+    elseif filereadable(resolve(expand("%:.:h") . "/" . submatch(1) . ".test" . submatch(3)))
+      return ".test"
+    else
+      return ""
+    endif
+  endfunction
+]],
+  false
+)
