4c1e980c5dcc89e3afc441214f140deca6145300
Karen Grigoryan <karen@karenjs.com>
2023-10-02 20:33:22 +0200

    feat: add telescope.lazy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,14 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    { "tsakirist/telescope-lazy.nvim" },
   },
   opts = {
     defaults = {
