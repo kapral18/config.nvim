21ba60b9c034f6e64c97ac286edb55dd191bc87e
Karen Grigoryan <karen@karenjs.com>
2023-10-07 09:14:36 +0200

    fix: popup blend and live_grep jump with update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,4 @@ vim.opt.conceallevel = 0 -- Do not hide * markup for bold and italic
 vim.opt.relativenumber = false -- Show relative line numbers
 vim.opt.wrap = true -- Enable line wrap
 vim.opt.breakindent = true -- Keep indentation on wrapped lines
+vim.opt.pumblend = 0 -- disable transparency in popup menu
