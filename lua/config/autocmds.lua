41ec0f4b1b11be707760eb96bdd3cbccfe4f6f8d
Karen Grigoryan <karen@karenjs.com>
2023-10-09 19:17:48 +0200

    feat: add inline blame and large file perf impr

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,27 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.wo.spell = true
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.wo.spell = true
+  end,
+})
+
+-- disable copilot for large files
+-- disable syntax highlighting for large files
+vim.api.nvim_create_autocmd({ "BufReadPre" }, {
+  pattern = { "*" },
+  callback = function()
+    local file = vim.fn.expand("<afile>")
+    local file_size = vim.fn.getfsize(file)
+    if file_size > 100000 or file_size == -2 then
+      vim.b.copilot_enabled = false
+      vim.wo.spell = false
+      vim.opt_local.synmaxcol = 250
+      -- disable some formatoptions
+      vim.opt_local.formatoptions:remove({ "a", "t" })
+    end
+  end,
+})
