b3eb56293a58905eabda5dc445455744bab77a7f
Karen Grigoryan <karen@karenjs.com>
2023-10-14 21:22:44 +0200

    feat: add json and markdown extras

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,16 +1,16 @@
-vim.filetype.add({
-  extension = {
-    mdx = "jsx",
-  },
-})
+-- vim.filetype.add({
+--   extension = {
+--     mdx = "jsx",
+--   },
+-- })
 
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "mdx-analyzer",
-      })
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "mdx-analyzer",
+  --     })
+  --   end,
+  -- },
 }
