fecc6ea302cf6f045c1536643dad538e1eefdc40
Karen Grigoryan <karen@karenjs.com>
2023-10-16 16:16:21 +0200

    feat: convert git-blame config to lua

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -2,9 +2,12 @@ return {
   {
     "f-person/git-blame.nvim",
     event = "BufRead",
-    config = function()
+    opts = {
+      enabled = false,
+    },
+    config = function(_, opts)
       vim.cmd("highlight default link gitblame SpecialComment")
-      vim.g.gitblame_enabled = 0
+      require("gitblame").setup(opts)
     end,
   },
 }
