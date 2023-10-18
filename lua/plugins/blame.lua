89e410f67850aaeb11910b4d53252f05dc3be7f3
Karen Grigoryan <karen@karenjs.com>
2023-10-18 08:19:02 +0200

    feat: disable blame when .git is not present

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -2,9 +2,12 @@ return {
   {
     "f-person/git-blame.nvim",
     event = "BufRead",
-    opts = {
-      enabled = false,
-    },
+    cond = function()
+      -- check if .git directory exists
+      local path = require("lazyvim.util").root.get() .. "/.git"
+      local ok, err = vim.loop.fs_stat(path)
+      return ok and ok.type == "directory"
+    end,
     config = function(_, opts)
       vim.cmd("highlight default link gitblame SpecialComment")
       require("gitblame").setup(opts)
