3957c92efd9ad5b86cd8b94463f45c07097eadb4
Karen Grigoryan <karen@karenjs.com>
2023-11-01 23:04:16 +0100

    feat: add yamlls !reference custom tag

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,4 +3,18 @@ return {
   dependencies = {
     { "antosha417/nvim-lsp-file-operations", config = true },
   },
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      yamlls = {
+        settings = {
+          yaml = {
+            customTags = {
+              "!reference sequence",
+            },
+          },
+        },
+      },
+    },
+  },
 }
