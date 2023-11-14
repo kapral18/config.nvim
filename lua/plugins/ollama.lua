54ef48b8c2584a4bdb37cdfa8ec5a2bfedb40284
Karen Grigoryan <karen@karenjs.com>
2023-11-14 21:54:53 +0100

    feat: change phind to codebooga a mix of wizard + phind ai model

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -4,6 +4,7 @@ return {
     { "<leader>]", ":Gen<CR>", mode = { "n", "v", "x" }, desc = "Local AI Gen" },
   },
   config = function()
-    require("gen").model = "phind-codellama:34b-v2"
+    -- require("gen").model = "phind-codellama:34b-v2"
+    require("gen").model = "codebooga"
   end,
 }
