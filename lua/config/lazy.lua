1b89b477b074df0772ffdd7fdcd606ac2c8a2044
Karen Grigoryan <karen@karenjs.com>
2024-01-05 00:54:10 +0100

    feat: tinker parallelism in lazy.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   change_detection = { enabled = false, notify = false },
+  concurrency = 100,
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
